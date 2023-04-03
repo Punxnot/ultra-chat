db96a045e715611b88673ebee81b865b2b60bf86
Marina Sulim <do-you-speak@yandex.ru>
2023-04-03 23:09:33 +0300

    Replace database url

--- index.js
+++ index.js
@@ -1,6 +1,121 @@
-var socket = io();
 $(() => {
-  var pattern = Trianglify({
+  const socket = io();
+
+  const addAllMessages = (data) => {
+    if (data && data.length) {
+      data.forEach(addMessage);
+      scrollToTheLast();
+    }
+  };
+
+  const addSingleMessage = (message) => {
+    addMessage(message);
+    scrollToTheLast();
+  };
+
+  const formatDate = (dateString) => {
+    const dt = new Date(dateString);
+    const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    return dt.toLocaleDateString('ru-RU', options);
+  };
+
+  const addMessage = (message) => {
+    formatDate(message.date);
+    message.message = handleLinks(message.message);
+    $("#messages").append(`<div class="single-message-container"><h4 class="message-username">${message.name}</h4> <p class="message-body">${message.message}</p><span class="message-date">${formatDate(message.date)}</span><button type="button" onclick="return replyToThis(event);">Ответить</button></div>`);
+  };
+
+  const replyToThis = (e) => {
+    let messageText = e.target.parentElement.querySelector('.message-body').textContent;
+    messageText = sanitize(messageText);
+    messageText = messageText.replace(/(\r\n\t|\n|\r\t)/gm, '');
+    const messageAuthor = e.target.parentElement.querySelector('.message-username').textContent;
+    const arr = messageText.match(/.{1,30}/g);
+    const resultText = `> ${messageAuthor}:\n`;
+
+    for (chunk of arr) {
+      resultText += `> ${chunk} \n`;
+    }
+
+    const textArea = document.getElementById('message');
+    textArea.value = resultText;
+    textArea.focus();
+  };
+
+  const getMessages = () => {
+    $.get('/messages', (data) => {
+      addAllMessages(data);
+    });
+  }
+
+  const sendMessage = (message) => {
+    $.post('/messages', message);
+  };
+
+  const scrollToTheLast = () => {
+    const allMessages = document.querySelectorAll('.message-body');
+    const lastMessage = allMessages[allMessages.length - 1];
+
+    if (lastMessage) {
+      lastMessage.scrollIntoView();
+    }
+  };
+
+  const sanitize = (string) => {
+    const output = string.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+  			 replace(/<[\/\!]*?[^<>]*?>/gi, '').
+  			 replace(/<style[^>]*?>.*?<\/style>/gi, '').
+  			 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '').
+         trim();
+    return output;
+  };
+
+  const handleLinks = (text) => {
+    const exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+    const text1 = text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
+    const exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    return text1.replace(exp2, '$1<a href="http://$2">$2</a>');
+  };
+
+  const handleFormSubmit = () => {
+    const nameInput = document.getElementById('name');
+    const messageInput = document.getElementById('message');
+    const sanitizedName = sanitize(nameInput.value);
+    const sanitizedMessage = sanitize(messageInput.value);
+
+    if (sanitizedName.length && sanitizedMessage.length) {
+      nameInput.classList.remove('error-field');
+      messageInput.classList.remove('error-field');
+      sendMessage({name: sanitizedName, message: sanitizedMessage});
+      messageInput.value = '';
+    } else if (!sanitizedName.length) {
+      nameInput.classList.add('error-field');
+    } else if (!sanitizedMessage.length) {
+      messageInput.classList.add('error-field');
+    }
+  };
+
+  const getCaret = (el) => {
+    if (el.selectionStart) {
+      return el.selectionStart;
+    } else if (document.selection) {
+      el.focus();
+      const r = document.selection.createRange();
+
+      if (r == null) {
+        return 0;
+      }
+
+      const re = el.createTextRange(), rc = re.duplicate();
+      re.moveToBookmark(r.getBookmark());
+      rc.setEndPoint('EndToStart', re);
+      return rc.text.length;
+    }
+
+    return 0;
+  };
+
+  const pattern = Trianglify({
     width: window.innerWidth,
     height: window.innerHeight
   });
@@ -8,12 +123,12 @@ $(() => {
   // Generate random pattern for background
   document.body.style.backgroundImage = 'url(' + pattern.png() + ')';
 
-  $("#message").keyup(function(event) {
+  $("#message").keyup((event) => {
     if (event.which == 13) {
-      var content = this.value;
-      var caret = getCaret(this);
-      if(event.shiftKey){
-        this.value = content.substring(0, caret - 1) + "\n" + content.substring(caret, content.length);
+      const content = this.value;
+      const caret = getCaret(this);
+      if (event.shiftKey) {
+        this.value = content.substring(0, caret - 1) + '\n' + content.substring(caret, content.length);
         event.stopPropagation();
       } else {
         this.value = content.substring(0, caret - 1) + content.substring(caret, content.length);
@@ -23,114 +138,6 @@ $(() => {
   });
 
   getMessages();
+
+  socket.on('message', addSingleMessage);
 });
-
-socket.on('message', addSingleMessage);
-
-function addAllMessages(data) {
-  if (data && data.length) {
-    data.forEach(addMessage);
-    scrollToTheLast();
-  }
-}
-
-function addSingleMessage(message) {
-  addMessage(message);
-  scrollToTheLast();
-}
-
-function formatDate(dateString) {
-  var dt = new Date(dateString);
-  var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-  return dt.toLocaleDateString('ru-RU', options);
-}
-
-function addMessage(message) {
-  formatDate(message.date);
-  message.message = handleLinks(message.message);
-  $("#messages").append(`<div class="single-message-container"><h4 class="message-username">${message.name}</h4> <p class="message-body">${message.message}</p><span class="message-date">${formatDate(message.date)}</span><button type="button" onclick="return replyToThis(event);">Ответить</button></div>`);
-}
-
-function replyToThis(e) {
-  var messageText = e.target.parentElement.querySelector('.message-body').textContent;
-  messageText = sanitize(messageText);
-  var messageAuthor = e.target.parentElement.querySelector('.message-username').textContent;
-
-  var messageText = messageText.replace(/(\r\n\t|\n|\r\t)/gm,"");
-  var arr = messageText.match(/.{1,30}/g);
-  var resultText = `> ${messageAuthor}:\n`;
-  for (chunk of arr) {
-    resultText += `> ${chunk} \n`;
-  }
-  var textArea = document.getElementById('message');
-  textArea.value = resultText;
-  textArea.focus();
-}
-
-function getMessages() {
-  $.get('/messages', (data) => {
-    addAllMessages(data);
-  })
-}
-
-function sendMessage(message) {
-  $.post('/messages', message)
-}
-
-function scrollToTheLast() {
-  var allMessages = document.querySelectorAll(".message-body");
-  var lastMessage = allMessages[allMessages.length - 1];
-  if (lastMessage) {
-    lastMessage.scrollIntoView();
-  }
-}
-
-function sanitize(string) {
-  var output = string.replace(/<script[^>]*?>.*?<\/script>/gi, '').
-			 replace(/<[\/\!]*?[^<>]*?>/gi, '').
-			 replace(/<style[^>]*?>.*?<\/style>/gi, '').
-			 replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '').
-       trim();
-  return output;
-}
-
-function handleLinks(text) {
-  var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-  var text1 = text.replace(exp, "<a target='_blank' href='$1'>$1</a>");
-  var exp2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-  return text1.replace(exp2, '$1<a href="http://$2">$2</a>');
-}
-
-function handleFormSubmit() {
-  var nameInput = document.getElementById('name');
-  var messageInput = document.getElementById('message');
-  var sanitizedName = sanitize(nameInput.value);
-  var sanitizedMessage = sanitize(messageInput.value);
-  if (sanitizedName.length && sanitizedMessage.length) {
-    nameInput.classList.remove('error-field');
-    messageInput.classList.remove('error-field');
-    sendMessage({name: sanitizedName, message: sanitizedMessage});
-    messageInput.value = '';
-  } else if (!sanitizedName.length) {
-    nameInput.classList.add('error-field');
-  } else if (!sanitizedMessage.length) {
-    messageInput.classList.add('error-field');
-  }
-}
-
-function getCaret(el) {
-  if (el.selectionStart) {
-    return el.selectionStart;
-  } else if (document.selection) {
-    el.focus();
-    var r = document.selection.createRange();
-    if (r == null) {
-      return 0;
-    }
-    var re = el.createTextRange(), rc = re.duplicate();
-    re.moveToBookmark(r.getBookmark());
-    rc.setEndPoint('EndToStart', re);
-    return rc.text.length;
-  }
-  return 0;
-}
--- server.js
+++ server.js
@@ -3,28 +3,24 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
 
-var express = require('express');
-var bodyParser = require('body-parser')
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var Message = mongoose.model('Message',{
+const Message = mongoose.model('Message',{
   name : String,
   message : String,
   date: { type: Date, default: Date.now }
 });
 
-// var dbUrl = `mongodb://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@ds125574.mlab.com:25574/chat2`;
-// mongodb+srv://ozma:<password>@cluster0.4ec6x.mongodb.net/<dbname>?retryWrites=true&w=majority
-
-// var dbUrl = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.4ec6x.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
-var dbUrl = `mongodb+srv://ozma:Sonrisa42Palabra66@chat2.cghlp.mongodb.net/chat2?retryWrites=true&w=majority`;
+const dbUrl = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@cluster0.4ec6x.mongodb.net/?retryWrites=true&w=majority`;
 
 app.get('/messages', (req, res) => {
   Message.find({}, null, {sort: {'_id': 1}}, (err, messages) => {
@@ -33,18 +29,21 @@ app.get('/messages', (req, res) => {
 });
 
 app.get('/messages/:user', (req, res) => {
-  var user = req.params.user;
+  const user = req.params.user;
+
   Message.find({name: user}, (err, messages) => {
     res.send(messages);
   });
 });
 
 app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
+  const message = new Message(req.body);
+
   message.save((err) => {
     if(err) {
       res.sendStatus(500);
     }
+
     req.body.date = message.date;
     io.emit('message', req.body);
     res.sendStatus(200);
@@ -56,11 +55,11 @@ io.on('connection', () => {
 });
 
 mongoose.connect(dbUrl, (err) => {
-  console.log('mongodb connected',err);
+  console.log('mongodb connected', err);
 });
 
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-var server = http.listen(port, () => {
+const server = http.listen(port, () => {
   console.log('Server is running on port', server.address().port);
 });
